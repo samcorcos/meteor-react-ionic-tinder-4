8c2b6f33d7a6dd59fbd325ec2eac08f673da6a42
Chet Corcos <ccorcos@gmail.com>
2015-08-19 12:22:14 -0700

    removing and rotating

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -1,4 +1,36 @@
+React.initializeTouchEvents(true)
+// Add listener to get :active pseudoselector working. hack
+document.addEventListener("touchstart", function(){}, false)
+// also need cursor:pointer to work on mobile to touch clic
+
+
+
 Home = React.createClass({
+  getInitialState() {
+    return {
+      cards: [{id:1}, {id:2}, {id:3}, {id:4}, {id:5}, {id:6}]
+    }
+  },
+  removeCard(id) {
+    this.setState({cards: this.state.cards.filter((card) =>  card.id != id )})
+  },
+  renderCards() {
+    return this.state.cards.map((card) => {
+      return <Card key= {card.id} card={card} remove={()=>this.removeCard(card.id)}/>
+    })
+  },
+  render() {
+    return (
+      <div>
+        {this.renderCards()}
+      </div>
+    )
+  }
+});
+
+
+
+Card = React.createClass({
   getInitialState() {
     return {
       x: 0,
@@ -30,15 +62,14 @@ Home = React.createClass({
         y: 0,
         dragging: "all 0.5s ease"
       })
+      Meteor.setTimeout(this.props.remove, 500)
     } else if (e.changedTouches[0].pageX > (window.innerWidth - 50)) {
       this.setState({
         x: 1000,
         y: 0,
         dragging: "all 0.5s ease"
       })
-      Meteor.defer(function() {
-        console.log("THIS IS WHERE I WANT TO DESTROY AFTER HALF A SECOND");
-      }, 500)
+      Meteor.setTimeout(this.props.remove, 500)
     } else {
       this.setState({
         x: 0,
@@ -48,17 +79,17 @@ Home = React.createClass({
     }
   },
   render() {
-    React.initializeTouchEvents(true)
     let cardStyle = {
       transform: "translate(" +
         this.state.x + "px," +
-        this.state.y + "px)",
+        this.state.y + "px) " + 
+        "rotate(" + this.state.x/4 + "deg)",
       transition: this.state.dragging
     }
     return (
       <div className="card" onTouchStart={this.moveCardInit} onTouchMove={this.moveCard} onTouchEnd={this.moveCardEnd} style={cardStyle}>
         <div className="item item-text-wrap">
-          This is a basic Card which contains an item that has wrapping text.
+          {"This is card id:" + this.props.card.id}
         </div>
       </div>
     )
