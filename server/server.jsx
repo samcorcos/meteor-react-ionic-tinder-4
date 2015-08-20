80a9c527b91005788807735144da2ac468c715c2
Sam Corcos <sam@corcos.io>
2015-08-20 12:10:44 +0200

    adding data

--- server/server.jsx
+++ server/server.jsx
@@ -1,3 +1,13 @@
 Meteor.publish("myData", function() {
   return MyData.find()
 })
+
+if (MyData.find().count() === 0) {
+  _.each(_.range(25), function() {
+    MyData.insert({
+      name: faker.name.findName(),
+      image: faker.image.people(),
+      details: faker.lorem.sentence()
+    })
+  })
+}
